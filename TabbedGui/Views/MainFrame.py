7947f23d7c3c379c5fd6abfdff72188b5ab75ba6
Timothy Canham <timcanham@users.noreply.github.com>
2017-06-22 20:11:14 -0700

    Child example

--- TabbedGui/Views/MainFrame.py
+++ TabbedGui/Views/MainFrame.py
@@ -12,3 +12,26 @@ class MainFrame(wx.Frame):
         self.panel = wx.Panel(self)
         self.panel.SetBackgroundColour(wx.BLACK)
         self.button = wx.Button(self.panel,label="Push Me",pos=(50,50))
+        self.btnId = self.button.GetId()
+        self.Bind(wx.EVT_BUTTON,self.OnButton,self.button)
+        
+    def OnButton(self,event):
+        """ Called when the Button is clicked"""
+        print("\nFrame GetChildren:")
+        for child in self.GetChildren():
+            print("%s" % repr(child))
+        print("\nPanel FindWindowById")
+        button = self.panel.FindWindowById(self.btnId)
+        print("%s"%repr(button))
+        button.SetLabel("Changed Label")
+        print("\nButton GetParent:")
+        panel = button.GetParent()
+        print("%s"%repr(panel))
+        print("\nGet the Application Object:")
+        app = wx.GetApp()
+        print("%s"%repr(app))
+        print("\nGet the Frame from the App:")
+        frame = app.GetTopWindow()
+        print("%s"%repr(frame))
+            
+        
